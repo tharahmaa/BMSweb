0596ec3ae89ec3a3ce7a3e6ff1f1c06ba6248c64
tharahmaa <143694936+tharahmaa@users.noreply.github.com>
2023-09-20 17:59:03 +0700

    Add files via upload

---
+++ Desktop1.vue
@@ -0,0 +1,789 @@
+<template>
+  <div class="desktop-1">
+    <img class="image-2-icon" alt="" src="/image-2@2x.png" />
+    <div class="rectangle-parent">
+      <div class="frame-child" />
+      <div class="meet-the-team">Meet the Team</div>
+    </div>
+    <div class="rectangle-group">
+      <div class="frame-child" />
+      <div class="our-artwork">Our Artwork</div>
+    </div>
+    <div class="rectangle-container">
+      <div class="frame-inner" />
+      <div class="more">More</div>
+    </div>
+    <div class="frame-div">
+      <div class="frame-inner" />
+      <div class="more">More</div>
+    </div>
+    <div class="group-div">
+      <div class="frame-inner" />
+      <div class="more">More</div>
+    </div>
+    <div class="rectangle-parent1">
+      <div class="frame-inner" />
+      <div class="more">More</div>
+    </div>
+    <div class="rectangle-parent2">
+      <div class="frame-inner" />
+      <div class="more">More</div>
+    </div>
+    <div class="rectangle-parent3">
+      <div class="group-child1" />
+      <div class="home">Home</div>
+    </div>
+    <div class="rectangle-parent4">
+      <div class="group-child1" />
+      <div class="home">Home</div>
+    </div>
+    <div class="rectangle-parent5">
+      <div class="group-child1" />
+      <div class="home">Home</div>
+    </div>
+    <div class="rectangle-parent6">
+      <div class="group-child1" />
+      <div class="home">Home</div>
+    </div>
+    <div class="rectangle-parent7">
+      <div class="group-child1" />
+      <div class="home">Home</div>
+    </div>
+    <div class="rectangle-parent8">
+      <div class="group-child6" />
+      <div class="more5">MORE</div>
+    </div>
+    <div class="team">
+      <div class="etha">
+        <div class="etha-child" />
+        <img
+          class="screen-shot-2021-12-08-at-133"
+          alt=""
+          src="/screen-shot-20211208-at-1339-2@2x.png"
+        />
+        <div class="ini-foto-kita-container">
+          <p class="ini">INI</p>
+          <p class="foto-kita">FOTO KITA</p>
+        </div>
+        <div class="ellipse-parent">
+          <div class="ellipse-div" />
+          <div class="group-child7" />
+          <div class="group-child8" />
+        </div>
+        <div class="etha1">etha</div>
+        <div class="etha-felisya-br">etha felisya br purba</div>
+      </div>
+      <div class="etha">
+        <div class="arka-child" />
+        <img
+          class="screen-shot-2021-12-08-at-133"
+          alt=""
+          src="/screen-shot-20211208-at-1339-2@2x.png"
+        />
+        <div class="ini-foto-kita-container">
+          <p class="ini">INI</p>
+          <p class="foto-kita">FOTO KITA</p>
+        </div>
+        <div class="ellipse-parent">
+          <div class="ellipse-div" />
+          <div class="group-child7" />
+          <div class="group-child8" />
+        </div>
+        <div class="etha1">arka</div>
+        <div class="m-arkananta-radithya">m. arkananta radithya</div>
+      </div>
+      <div class="etha">
+        <div class="etha-child" />
+        <img
+          class="screen-shot-2021-12-08-at-133"
+          alt=""
+          src="/screen-shot-20211208-at-1339-2@2x.png"
+        />
+        <div class="ini-foto-kita-container">
+          <p class="ini">INI</p>
+          <p class="foto-kita">FOTO KITA</p>
+        </div>
+        <div class="ellipse-parent">
+          <div class="ellipse-div" />
+          <div class="group-child7" />
+          <div class="group-child8" />
+        </div>
+        <div class="etha1">atha</div>
+        <div class="etha-felisya-br">atha rahma arianti</div>
+      </div>
+      <div class="etha">
+        <div class="etha-child" />
+        <img
+          class="screen-shot-2021-12-08-at-133"
+          alt=""
+          src="/screen-shot-20211208-at-1339-2@2x.png"
+        />
+        <div class="ini-foto-kita-container">
+          <p class="ini">INI</p>
+          <p class="foto-kita">FOTO KITA</p>
+        </div>
+        <div class="ellipse-parent">
+          <div class="ellipse-div" />
+          <div class="group-child7" />
+          <div class="group-child8" />
+        </div>
+        <div class="etha1">imam</div>
+        <div class="etha-felisya-br">imam nurhadi</div>
+      </div>
+    </div>
+    <div class="navbar">
+      <div class="navbar-child" />
+      <div class="frame-parent">
+        <div class="group-parent">
+          <div class="ellipse-parent2">
+            <div class="group-child18" />
+            <div class="group-child19" />
+            <div class="group-child20" />
+          </div>
+          <div class="nama-website">NAMA WEBSITE</div>
+        </div>
+        <div class="home-parent">
+          <div class="nama-website">HOME</div>
+          <div class="nama-website">TEAM</div>
+          <div class="artwork">ARTWORK</div>
+        </div>
+      </div>
+    </div>
+    <div class="welcome-to-our">welcome to our website!</div>
+    <div class="fotografi">FOTOGRAFI</div>
+    <div class="karya-tulis">KARYA TULIS</div>
+    <div class="web-development">WEB DEVELOPMENT</div>
+    <div class="jurnalistik">JURNALISTIK</div>
+    <div class="uiux-design">UI/UX DESIGN</div>
+    <img
+      class="d6d7ac59-3e16-410a-a27d-71990c-icon"
+      alt=""
+      src="/d6d7ac593e16410aa27d71990cf688c6-1@2x.png"
+    />
+    <div class="fotografi1">FOTOGRAFI</div>
+    <div class="fotografi-adalah-seni">
+      Fotografi adalah seni menangkap keindahan dan momen berharga dalam satu
+      frame. Dengan kamera, kita bisa berbicara tanpa kata-kata dan membagikan
+      kisah visual yang mendalam. Berikut adalah beberapa karya kami di bidang
+      fotografi.
+    </div>
+    <div class="karya-tulis-adalah">
+      Karya tulis adalah cara untuk menyampaikan pemikiran, cerita, atau
+      informasi melalui tulisan. Ini adalah medium ekspresi yang kuat dan
+      beragam, termasuk dalam bentuk fiksi, nonfiksi, esai, atau puisi, yang
+      memungkinkan penulis untuk berkomunikasi dengan pembaca melalui kata-kata
+      tertulis. Berikut adalah beberapa karya kami di bidang karya tulis.
+    </div>
+    <div class="web-development-adalah">
+      Web development adalah proses pembuatan dan pemeliharaan situs web. Ini
+      melibatkan pengembangan tampilan antarmuka, fungsionalitas, dan database
+      untuk menciptakan situs web yang responsif dan interaktif. Berikut adalah
+      beberapa karya kami di bidang web development.
+    </div>
+    <img class="image-6-icon" alt="" src="/image-6@2x.png" />
+    <div class="jurnalistik-adalah-profesi">
+      Jurnalistik adalah profesi yang bertugas menyampaikan informasi objektif
+      dan akurat kepada masyarakat tentang berita, peristiwa, dan isu-isu
+      terkini. Para jurnalis mencari fakta, wawancara sumber, dan menyajikan
+      cerita yang relevan, berperan penting dalam menjaga demokrasi dan
+      memeriksa kekuasaan. Berikut adalah beberapa karya kami di bidang
+      jurnalistik.
+    </div>
+    <div class="uiux-design-adalah">
+      UI/UX design adalah elemen penting dalam pengembangan produk digital. UI
+      berkaitan dengan tampilan visual, sementara UX menekankan pada pengalaman
+      pengguna secara keseluruhan. Keduanya bekerja bersama untuk menciptakan
+      produk yang menarik, efisien, dan memuaskan pengguna. Berikut adalah
+      beberapa karya kami di bidang ui/ux.
+    </div>
+    <img class="wawancara-1-icon" alt="" src="/wawancara-1@2x.png" />
+    <img
+      class="kertas-buku-tulis-kosong-f10-1"
+      alt=""
+      src="/kertas-buku-tulis-kosong-f10-1@2x.png"
+    />
+    <img
+      class="project-management-mobile-appl"
+      alt=""
+      src="/project-management-mobile-application-ui-design-1@2x.png"
+    />
+  </div>
+</template>
+<script lang="ts">
+  import { defineComponent, ref } from "vue";
+
+  export default defineComponent({
+    name: "Desktop1",
+  });
+</script>
+<style scoped>
+  .image-2-icon {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 1440px;
+    height: 827px;
+    object-fit: cover;
+  }
+  .frame-child {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    border-radius: var(--br-1521xl);
+    background-color: var(--color-forestgreen);
+    width: 364px;
+    height: 75px;
+  }
+  .meet-the-team {
+    position: absolute;
+    top: 18px;
+    left: 60px;
+    font-weight: 500;
+  }
+  .rectangle-parent {
+    position: absolute;
+    top: 900px;
+    left: 538px;
+    width: 364px;
+    height: 75px;
+    color: var(--color-gray-300);
+  }
+  .our-artwork {
+    position: absolute;
+    top: 18px;
+    left: 81px;
+    font-weight: 500;
+  }
+  .rectangle-group {
+    position: absolute;
+    top: 1486px;
+    left: 538px;
+    width: 364px;
+    height: 75px;
+    color: var(--color-gray-300);
+  }
+  .frame-inner {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    border-radius: var(--br-1521xl);
+    background-color: var(--color-gray-200);
+    border: 1px solid var(--color-gray-100);
+    box-sizing: border-box;
+    width: 203px;
+    height: 75px;
+  }
+  .more {
+    position: absolute;
+    top: 18px;
+    left: 58px;
+    font-weight: 600;
+    display: inline-block;
+    width: 88px;
+  }
+  .rectangle-container {
+    position: absolute;
+    top: 2073px;
+    left: 564px;
+    width: 203px;
+    height: 75px;
+    color: var(--color-gray-100);
+  }
+  .frame-div {
+    position: absolute;
+    top: 3302px;
+    left: 565px;
+    width: 203px;
+    height: 75px;
+    color: var(--color-gray-100);
+  }
+  .group-div {
+    position: absolute;
+    top: 4633px;
+    left: 563px;
+    width: 203px;
+    height: 75px;
+    color: var(--color-gray-100);
+  }
+  .rectangle-parent1 {
+    position: absolute;
+    top: 2683px;
+    left: 125px;
+    width: 203px;
+    height: 75px;
+    color: var(--color-gray-100);
+  }
+  .rectangle-parent2 {
+    position: absolute;
+    top: 3960px;
+    left: 125px;
+    width: 203px;
+    height: 75px;
+    color: var(--color-gray-100);
+  }
+  .group-child1 {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    border-radius: var(--br-1521xl);
+    background-color: var(--color-forestgreen);
+    width: 198px;
+    height: 75px;
+  }
+  .home {
+    position: absolute;
+    top: 18px;
+    left: 46px;
+    font-weight: 600;
+    display: inline-block;
+    width: 105px;
+  }
+  .rectangle-parent3 {
+    position: absolute;
+    top: 2073px;
+    left: 806px;
+    width: 198px;
+    height: 75px;
+    color: var(--color-gray-200);
+  }
+  .rectangle-parent4 {
+    position: absolute;
+    top: 3302px;
+    left: 807px;
+    width: 198px;
+    height: 75px;
+    color: var(--color-gray-200);
+  }
+  .rectangle-parent5 {
+    position: absolute;
+    top: 4633px;
+    left: 805px;
+    width: 198px;
+    height: 75px;
+    color: var(--color-gray-200);
+  }
+  .rectangle-parent6 {
+    position: absolute;
+    top: 2683px;
+    left: 367px;
+    width: 198px;
+    height: 75px;
+    color: var(--color-gray-200);
+  }
+  .rectangle-parent7 {
+    position: absolute;
+    top: 3960px;
+    left: 367px;
+    width: 198px;
+    height: 75px;
+    color: var(--color-gray-200);
+  }
+  .group-child6 {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    border-radius: var(--br-mini);
+    background-color: var(--color-forestgreen);
+    width: 99px;
+    height: 35px;
+  }
+  .more5 {
+    position: absolute;
+    top: 9px;
+    left: 24px;
+    letter-spacing: 0.15em;
+    font-weight: 500;
+  }
+  .rectangle-parent8 {
+    position: absolute;
+    top: 423px;
+    left: 670px;
+    width: 99px;
+    height: 35px;
+    font-size: 14px;
+    color: var(--color-gray-300);
+  }
+  .etha-child {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    border-radius: var(--br-mini);
+    background-color: var(--color-gray-200);
+    width: 217px;
+    height: 324px;
+  }
+  .screen-shot-2021-12-08-at-133 {
+    position: absolute;
+    top: 24px;
+    left: 26px;
+    border-radius: var(--br-mini);
+    width: 165px;
+    height: 155px;
+    object-fit: cover;
+  }
+  .ini {
+    margin-block-start: 0;
+    margin-block-end: 3px;
+  }
+  .foto-kita {
+    margin: 0;
+  }
+  .ini-foto-kita-container {
+    position: absolute;
+    top: 58px;
+    left: 32px;
+    font-size: var(--font-size-17xl);
+    letter-spacing: -0.03em;
+    line-height: 28px;
+    font-weight: 800;
+    display: inline-block;
+    width: 154px;
+  }
+  .ellipse-div {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    top: 0%;
+    right: 0%;
+    bottom: 0%;
+    left: 0%;
+    border-radius: 50%;
+    background-color: var(--color-springgreen);
+  }
+  .group-child7 {
+    position: absolute;
+    height: 35.36%;
+    width: 8.93%;
+    top: 32.14%;
+    right: 55.36%;
+    bottom: 32.5%;
+    left: 35.71%;
+    border-radius: var(--br-mini);
+    background-color: var(--color-gray-300);
+  }
+  .group-child8 {
+    position: absolute;
+    height: 35.36%;
+    width: 8.93%;
+    top: 32.14%;
+    right: 35.71%;
+    bottom: 32.5%;
+    left: 55.36%;
+    border-radius: var(--br-mini);
+    background-color: var(--color-gray-300);
+  }
+  .ellipse-parent {
+    position: absolute;
+    height: 10.97%;
+    width: 16.59%;
+    top: 41.67%;
+    right: 15.67%;
+    bottom: 47.36%;
+    left: 67.74%;
+  }
+  .etha1 {
+    position: absolute;
+    top: 192px;
+    left: 26px;
+    letter-spacing: -0.03em;
+    line-height: 28px;
+    font-weight: 800;
+    color: var(--color-white);
+  }
+  .etha-felisya-br {
+    position: absolute;
+    top: 220px;
+    left: 26px;
+    letter-spacing: -0.03em;
+    line-height: 28px;
+    font-weight: 500;
+    color: var(--color-darkgray);
+  }
+  .etha {
+    position: relative;
+    width: 217px;
+    height: 324px;
+  }
+  .arka-child {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    border-radius: var(--br-mini);
+    background-color: #282828;
+    width: 217px;
+    height: 324px;
+  }
+  .m-arkananta-radithya {
+    position: absolute;
+    top: 220px;
+    left: 26px;
+    letter-spacing: -0.03em;
+    line-height: 28px;
+    font-weight: 500;
+    color: var(--color-darkgray);
+    text-align: left;
+  }
+  .team {
+    position: absolute;
+    top: 1048px;
+    left: 30px;
+    width: 1380px;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: center;
+    gap: 118px;
+    text-align: center;
+    font-size: var(--font-size-base);
+    color: var(--color-gray-300);
+  }
+  .navbar-child {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    border-radius: var(--br-mini);
+    background-color: var(--color-gray-300);
+    width: 1406px;
+    height: 35px;
+  }
+  .group-child18 {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    top: 0%;
+    right: 0%;
+    bottom: 0%;
+    left: 0%;
+    border-radius: 50%;
+    background-color: #283f49;
+  }
+  .group-child19 {
+    position: absolute;
+    height: 35.36%;
+    width: 8.93%;
+    top: 32.14%;
+    right: 55.36%;
+    bottom: 32.5%;
+    left: 35.71%;
+    background-color: var(--color-springgreen);
+  }
+  .group-child20 {
+    position: absolute;
+    height: 35.36%;
+    width: 8.93%;
+    top: 32.14%;
+    right: 35.71%;
+    bottom: 32.5%;
+    left: 55.36%;
+    background-color: var(--color-springgreen);
+  }
+  .ellipse-parent2 {
+    position: relative;
+    width: 24.31px;
+    height: 24px;
+  }
+  .nama-website {
+    position: relative;
+    letter-spacing: 0.15em;
+    font-weight: 500;
+  }
+  .group-parent {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 19px;
+  }
+  .artwork {
+    position: relative;
+    letter-spacing: 0.15em;
+    font-weight: 500;
+    display: inline-block;
+    width: 145px;
+    flex-shrink: 0;
+  }
+  .home-parent {
+    width: 275px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 43px;
+  }
+  .frame-parent {
+    position: absolute;
+    top: 5px;
+    left: 25px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 899px;
+  }
+  .navbar {
+    position: absolute;
+    top: 14px;
+    left: 17px;
+    width: 1406px;
+    height: 35px;
+    font-size: var(--font-size-base);
+  }
+  .welcome-to-our {
+    position: absolute;
+    top: 318px;
+    left: 299px;
+    font-size: 64px;
+    font-weight: 800;
+  }
+  .fotografi {
+    position: absolute;
+    top: 1734px;
+    left: 564px;
+    font-size: var(--font-size-41xl);
+    font-weight: 800;
+  }
+  .karya-tulis {
+    position: absolute;
+    top: 2963px;
+    left: 565px;
+    font-size: var(--font-size-41xl);
+    font-weight: 800;
+  }
+  .web-development {
+    position: absolute;
+    top: 4294px;
+    left: 563px;
+    font-size: var(--font-size-41xl);
+    font-weight: 800;
+  }
+  .jurnalistik {
+    position: absolute;
+    top: 2366px;
+    left: 125px;
+    font-size: var(--font-size-41xl);
+    font-weight: 800;
+  }
+  .uiux-design {
+    position: absolute;
+    top: 3643px;
+    left: 125px;
+    font-size: var(--font-size-41xl);
+    font-weight: 800;
+  }
+  .d6d7ac59-3e16-410a-a27d-71990c-icon {
+    position: absolute;
+    top: 1705px;
+    left: 75px;
+    border-radius: var(--br-31xl);
+    width: 450px;
+    height: 443px;
+    object-fit: cover;
+  }
+  .fotografi1 {
+    position: absolute;
+    top: 2063px;
+    left: 102px;
+    font-size: 30px;
+    font-weight: 800;
+    display: inline-block;
+    width: 198px;
+    height: 37px;
+  }
+  .fotografi-adalah-seni {
+    position: absolute;
+    top: 1823px;
+    left: 564px;
+    font-size: var(--font-size-6xl);
+    font-weight: 600;
+    display: inline-block;
+    width: 761px;
+    height: 153px;
+  }
+  .karya-tulis-adalah {
+    position: absolute;
+    top: 3052px;
+    left: 565px;
+    font-size: var(--font-size-6xl);
+    font-weight: 600;
+    display: inline-block;
+    width: 761px;
+    height: 214px;
+  }
+  .web-development-adalah {
+    position: absolute;
+    top: 4383px;
+    left: 563px;
+    font-size: var(--font-size-6xl);
+    font-weight: 600;
+    display: inline-block;
+    width: 761px;
+    height: 214px;
+  }
+  .image-6-icon {
+    position: absolute;
+    top: 4279px;
+    left: 56px;
+    border-radius: var(--br-31xl);
+    width: 450px;
+    height: 320.64px;
+    object-fit: cover;
+  }
+  .jurnalistik-adalah-profesi {
+    position: absolute;
+    top: 2455px;
+    left: 125px;
+    font-size: var(--font-size-6xl);
+    font-weight: 600;
+    display: inline-block;
+    width: 761px;
+    height: 210px;
+  }
+  .uiux-design-adalah {
+    position: absolute;
+    top: 3732px;
+    left: 125px;
+    font-size: var(--font-size-6xl);
+    font-weight: 600;
+    display: inline-block;
+    width: 761px;
+    height: 210px;
+  }
+  .wawancara-1-icon {
+    position: absolute;
+    top: calc(50% - 439px);
+    left: 905px;
+    border-radius: var(--br-31xl);
+    width: 450px;
+    height: 439px;
+    object-fit: cover;
+  }
+  .kertas-buku-tulis-kosong-f10-1 {
+    position: absolute;
+    top: 2901px;
+    left: 81px;
+    border-radius: var(--br-31xl);
+    width: 450px;
+    height: 450px;
+    object-fit: cover;
+  }
+  .project-management-mobile-appl {
+    position: absolute;
+    top: 3585px;
+    left: 906px;
+    border-radius: var(--br-31xl);
+    width: 450px;
+    height: 450px;
+    object-fit: cover;
+  }
+  .desktop-1 {
+    position: relative;
+    background-color: #121212;
+    width: 100%;
+    height: 5516px;
+    overflow: hidden;
+    text-align: left;
+    font-size: var(--font-size-13xl);
+    color: var(--color-white);
+    font-family: var(--font-montserrat);
+  }
+</style>
